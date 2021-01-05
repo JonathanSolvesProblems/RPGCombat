62957b580d602383b91c011e5519fe82df28cb21
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2021-01-05 12:07:56 -0500

    clickable movement implemented.

--- RPGCombatGame/Assets/Scripts/Mover.cs
+++ RPGCombatGame/Assets/Scripts/Mover.cs
@@ -6,9 +6,33 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {   
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit); // getting position info where the raycast hit. - Raycast method bool
+
+        if (hasHit)
+        {
+            agent.destination = hit.point;
+        }
+
+
     }
 }
